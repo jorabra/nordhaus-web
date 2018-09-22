c587231cb9fbcd96584517c89468d82efee90d0f
Jørgen Abrahamsen <jorgen.abrahamsen@gmail.com>
2018-09-22 18:16:44 +0200

    Add Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,27 @@ module.exports = {
           `Fira+Mono\:400,700`
         ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-126218644-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "",
+        // Any additional create only fields (optional)
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        cookieDomain: "nordhaus.no"
+      }
     }
+
   ],
 }
